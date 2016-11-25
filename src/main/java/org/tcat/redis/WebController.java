182d77f4b53493e0dbbe8d32e7fe68d8e7f5ff7e
LambdaExpression <13425088053@139.com>
2016-11-25 15:00:26 +0800

    Update WebController.java

--- src/main/java/org/tcat/redis/WebController.java
+++ src/main/java/org/tcat/redis/WebController.java
@@ -42,7 +42,7 @@ public class WebController {
     @ResponseBody
     public Object logout(HttpServletRequest request) {
         HttpSession session = request.getSession();
-        session.removeAttribute(USER);
+        session.invalidate();
         return true;
     }
 
